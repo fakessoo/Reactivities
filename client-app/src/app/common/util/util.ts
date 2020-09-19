1ceca43de39989881fa9918082df9856ecf31c86
fakessoo <fakessoo83@gmail.com>
2020-09-18 18:55:04 -0700

    End of section 15

--- client-app/src/app/common/util/util.ts
+++ client-app/src/app/common/util/util.ts
@@ -1,3 +1,6 @@
+import { IUser } from "../../models/user";
+import { IActivity, IAttendee } from "../../models/activity";
+
 export const combineDateAndTime = (date: Date, time: Date) =>
 {
     const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
@@ -9,3 +12,29 @@ export const combineDateAndTime = (date: Date, time: Date) =>
 
     return new Date(dateString + ' ' + timeString);
 } 
+
+export const setActivityProps = (activity: IActivity, user: IUser) => {
+    activity.date = new Date(activity.date);
+    activity.isGoing = activity.attendees.some(
+      a => a.username === user.username
+    )
+
+    // activity.attendees.forEach(atdee => {
+    //     console.log("atdee.username : " + atdee.username + " / user.name : " + user.username);
+    // });
+
+
+    activity.isHost = activity.attendees.some(
+      a => a.username === user.username && a.isHost
+    );
+    return activity;
+};
+
+export const createAttendee = (user: IUser): IAttendee => {
+  return {
+    displayName: user.displayName,
+    isHost: false,
+    username: user.username,
+    image: user.image!
+  }
+}
